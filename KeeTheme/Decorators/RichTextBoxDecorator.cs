5bcbe2ffb6005a3bb60f7b08ea9f88adfecdd998
Krzysztof Łaputa <Krzysztof.Laputa@logsystem.pl>
2019-07-08 22:00:33 +0200

    Fix #10 missing text in RichTextBox

--- KeeTheme/Decorators/RichTextBoxDecorator.cs
+++ KeeTheme/Decorators/RichTextBoxDecorator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 using KeeTheme.Theme;
 
@@ -29,6 +30,7 @@ namespace KeeTheme.Decorators
 			parent.Controls.SetChildIndex(this, index);
 			BorderStyle = _theme.RichTextBox.BorderStyle;
 			richTextBox.BorderStyle = BorderStyle.None;
+			richTextBox.Location = Point.Empty;
 			Controls.Add(richTextBox);
 			EnabledChanged += HandleEnabledChanged;
 
