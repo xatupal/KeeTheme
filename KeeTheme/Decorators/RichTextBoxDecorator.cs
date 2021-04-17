0ee819d5b4aaf2500d6cac57445f0c85b56be8c1
Krzysztof Łaputa <xatupal@nibiru.pl>
2021-04-17 22:59:05 +0200

    Fix color of links in the entry view

--- KeeTheme/Decorators/RichTextBoxDecorator.cs
+++ KeeTheme/Decorators/RichTextBoxDecorator.cs
@@ -1,15 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 using KeePass.Forms;
 using KeePass.UI;
+using KeePassLib.Utility;
 using KeeTheme.Theme;
 
 namespace KeeTheme.Decorators
 {
 	sealed class RichTextBoxDecorator : Panel
 	{
+		private class Link
+		{
+			public int Index { get; set; }
+			public string Text { get; set; }
+		}
+
+		private readonly List<Link> _detectedLinks = new List<Link>();
+		private string _lastText;
+		
 		private readonly RichTextBox _richTextBox;
+		private readonly RichTextBoxNativeWindow _richTextBoxNativeWindow;
 
 		private ITheme _theme;
 		private bool _enabled;
@@ -39,6 +51,9 @@ namespace KeeTheme.Decorators
 			if (Parent.GetType() != typeof(DataEditorForm))
 			{
 				richTextBox.TextChanged += HandleRichTextBoxTextChanged;
+				_richTextBoxNativeWindow = new RichTextBoxNativeWindow(richTextBox);
+				_richTextBoxNativeWindow.Paint += HandleRichTextBoxPaint;
+				_richTextBoxNativeWindow.LinkCreated += HandleRichTextBoxLinkCreated;
 			}
 			else
 			{
@@ -48,6 +63,89 @@ namespace KeeTheme.Decorators
 					richTextBox.TextChanged += HandleRichTextBoxTextChanged;
 			}
 			richTextBox.DockChanged += HandleRichTextBoxDockChanged;
+		}
+
+		private void HandleRichTextBoxLinkCreated(object sender, EventArgs e)
+		{
+			var customRichTextBox = sender as CustomRichTextBoxEx;
+			if (customRichTextBox == null)
+				return;
+
+			if (_lastText != customRichTextBox.Text)
+			{
+				_lastText = customRichTextBox.Text;
+				_detectedLinks.Clear();
+			}
+
+			var link = new Link();
+			link.Index = customRichTextBox.SelectionStart;
+			link.Text = customRichTextBox.Text.Substring(link.Index, customRichTextBox.SelectionLength);
+			_detectedLinks.Add(link);
+		}
+
+		private void HandleRichTextBoxPaint(object sender, PaintEventArgs e)
+		{
+			var customRichTextBox = sender as CustomRichTextBoxEx;
+			if (customRichTextBox == null)
+				return;
+
+			using (var font = new Font(customRichTextBox.Font, FontStyle.Underline))
+			{
+				foreach (var link in _detectedLinks)
+				{
+					DrawLink(customRichTextBox, link, e.Graphics, font);
+				}
+			}
+		}
+
+		private void DrawLink(CustomRichTextBoxEx customRichTextBox, Link link, Graphics graphics, Font font)
+		{
+			var ranges = Subtract(link.Index, link.Text.Length, customRichTextBox.SelectionStart,
+				customRichTextBox.SelectionLength);
+
+			foreach (var range in ranges)
+			{
+				var linkText = link.Text.Substring(range.First - link.Index, range.Length);
+				var startPoint = customRichTextBox.GetPositionFromCharIndex(range.First);
+				var startPointPadded = new Point(startPoint.X - 3, startPoint.Y);
+				TextRenderer.DrawText(graphics, linkText, font, startPointPadded, _theme.LinkLabel.LinkColor);
+			}
+		}
+
+		private List<CharacterRange> Subtract(int linkStart, int linkLength, int selectionStart, int selectionLength)
+		{
+			var linkEnd = linkStart + linkLength;
+			var selectionEnd = selectionStart + selectionLength;
+
+			// Empty selection or selection not in range of link
+			if (selectionLength == 0 || linkStart > selectionEnd || linkEnd < selectionStart)
+			{
+				var range = new CharacterRange(linkStart, linkLength);
+				return new List<CharacterRange> { range };
+			}
+			
+			// Selection overlaps whole link
+			if (selectionStart <= linkStart && selectionEnd >= linkEnd)
+				return new List<CharacterRange>();
+
+			// Selection starts before link and ends inside link
+			if (selectionStart <= linkStart && selectionEnd < linkEnd)
+			{
+				var result = new CharacterRange(selectionEnd, linkEnd - selectionEnd);
+				return new List<CharacterRange> { result };
+			}
+
+			// Selection starts inside link and ends after link
+			if (selectionStart > linkStart && selectionEnd >= linkEnd)
+			{
+				var result = new CharacterRange(linkStart, selectionStart - linkStart);
+				return new List<CharacterRange> { result };
+			}
+
+			// Selection is inside link
+			var result1 = new CharacterRange(linkStart, selectionStart - linkStart);
+			var result2 = new CharacterRange(selectionEnd, linkEnd - selectionEnd);
+			return new List<CharacterRange> { result1, result2 };
 		}
 
 		private void HandleRichTextBoxDockChanged(object sender, EventArgs e)
@@ -73,6 +171,8 @@ namespace KeeTheme.Decorators
 			richTextBox.SelectAll();
 			richTextBox.SelectionColor = _theme.RichTextBox.SelectionColor;
 			richTextBox.Select(selectionStart, selectionLength);
+			if (richTextBox.TextLength == 0)
+				_detectedLinks.Clear();
 		}
 
 		public void EnableTheme(bool enabled, ITheme theme)
---
+++ KeeTheme/Decorators/RichTextBoxNativeWindow.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace KeeTheme.Decorators
+{
+    class RichTextBoxNativeWindow : NativeWindow
+    {
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        internal struct CHARFORMAT2
+        {
+            public UInt32 cbSize;
+            public UInt32 dwMask;
+            public UInt32 dwEffects;
+            public Int32 yHeight;
+            public Int32 yOffset;
+            public UInt32 crTextColor;
+            public Byte bCharSet;
+            public Byte bPitchAndFamily;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string szFaceName;
+
+            public UInt16 wWeight;
+            public UInt16 sSpacing;
+            public Int32 crBackColor;
+            public Int32 lcid;
+            public UInt32 dwReserved;
+            public Int16 sStyle;
+            public Int16 wKerning;
+            public Byte bUnderlineType;
+            public Byte bAnimation;
+            public Byte bRevAuthor;
+            public Byte bReserved1;
+        }
+        
+        internal const int WM_PAINT = 0x000F;
+        internal const int WM_USER = 0x0400;
+        internal const int EM_SETCHARFORMAT = WM_USER + 68;
+        internal const uint CFE_LINK = 0x0020;
+        
+        private readonly RichTextBox _richTextBox;
+        
+        internal event PaintEventHandler Paint;
+        internal event EventHandler LinkCreated; 
+
+        public RichTextBoxNativeWindow(RichTextBox richTextBox)
+        {
+            _richTextBox = richTextBox;
+            _richTextBox.HandleCreated += HandleRichTextBoxHandleCreated;
+            _richTextBox.HandleDestroyed += HandleRichTextBoxHandleDestroyed;
+            AssignHandle(_richTextBox.Handle);
+        }
+
+        private void HandleRichTextBoxHandleCreated(object sender, EventArgs e)
+        {
+            if (_richTextBox != null) 
+                AssignHandle(_richTextBox.Handle);
+        }
+
+        private void HandleRichTextBoxHandleDestroyed(object sender, EventArgs e)
+        {
+            ReleaseHandle();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == EM_SETCHARFORMAT)
+            {
+                var cf = (CHARFORMAT2) Marshal.PtrToStructure(m.LParam, typeof(CHARFORMAT2));
+                if ((cf.dwEffects & CFE_LINK) != 0)
+                {
+                    var args = new EventArgs();
+                    OnLinkCreated(args);
+                }
+            }
+            
+            if (m.Msg == WM_PAINT)
+            {
+                using (var g = Graphics.FromHwnd(m.HWnd))
+                {
+                    var rect = new Rectangle(0, 0, _richTextBox.ClientSize.Width, _richTextBox.ClientSize.Height);
+                    var args = new PaintEventArgs(g, rect);
+                    OnPaint(args);
+                }
+            }
+        }
+
+        protected virtual void OnLinkCreated(EventArgs e)
+        {
+            if (LinkCreated != null)
+                LinkCreated.Invoke(_richTextBox, e);
+        }
+        
+        protected virtual void OnPaint(PaintEventArgs e)
+        {
+            if (Paint != null)
+                Paint.Invoke(_richTextBox, e);
+        }
+    }
+}
